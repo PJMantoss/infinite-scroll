c715b3bca50b71f008c0ab7eda2bf2e7474faf0a
PJMantoss <joelptoss@gmail.com>
2022-01-28 22:47:58 +0100

    update script

--- scripts.js
+++ scripts.js
@@ -45,5 +45,5 @@ async function fetchAndDisplayTestimonials(){
 
 function createTestimonialElement(message){
     const testimonialElement = document.createElement('p');
-    
+    testimonialElement.classList.add('testimonial');
 }
