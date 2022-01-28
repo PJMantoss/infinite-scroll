f5ed56bfdd98939aa17c07dd4cf494384c08e4ee
PJMantoss <joelptoss@gmail.com>
2022-01-28 22:54:25 +0100

    update script

--- scripts.js
+++ scripts.js
@@ -53,4 +53,8 @@ function createTestimonialElement(message){
 function createTestimonialUrl(){
     const url = new URL(API_BASE_URL);
     url.searchParams.set('limit', PAGE_SIZE);
+
+    if(afterId !== null){
+        url.searchParams.set();
+    }
 };
